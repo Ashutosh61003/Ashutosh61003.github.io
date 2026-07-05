8ef644aaec8d15dab521839a55ef6c1989679b8c
AshutoshSri123 <ashu.61003@gmail.com>
2026-07-05 16:26:16 +0530

    Serve built app from Pages root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: 'src',
+    publicDir: '../public',
     plugins: [react()],
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
 })
